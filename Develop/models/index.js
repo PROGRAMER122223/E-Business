830a36646af3cea7d1dd1f188eb00975e066fd3c
Prathibha <pratibha.indel@gmail.com>
2022-03-25 09:40:03 -0400

    updated index.js for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+//onDelete: 'SET NULL'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+    //unique: false,
+    foreignkey:'product_id'
+  //},
+  //as: 'products_tags'
+  
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+    //unique: false
+    foreignkey:'tag_id'
+ // },
+ // as: 'tagged_products'
+  });
+
 
 module.exports = {
   Product,
